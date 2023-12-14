715cfce07fa082feca0a3d0d6bd79755c3247a1e
Tom <blak1002@gmail.com>
2023-12-14 19:40:01 +0900

    [config] Jetpack-Navigation 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,6 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     // Coil
     implementation("io.coil-kt:coil-compose:2.5.0")
+    //Jetpack-Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.5")
 }
