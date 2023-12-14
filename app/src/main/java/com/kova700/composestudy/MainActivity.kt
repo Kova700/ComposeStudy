927ad2ff0ab76cccffb410abb766c83411fd7b5d
Tom <blak1002@gmail.com>
2023-12-14 13:25:11 +0900

    BOX 예제

--- app/src/main/java/com/kova700/composestudy/MainActivity.kt
+++ app/src/main/java/com/kova700/composestudy/MainActivity.kt
@@ -8,13 +8,16 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -47,8 +50,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeStudyTheme {
-//                MyImageTest()
-                MyImageTest2()
+                BoxTest()
             }
         }
     }
@@ -58,9 +60,114 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingPreview() {
     ComposeStudyTheme {
-//        MyImageTest()
-        MyImageTest2()
+        BoxTest()
     }
+}
+
+@Composable
+fun BoxTest() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Red)
+    ) {
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Blue)
+                .padding(16.dp)
+                .align(Alignment.TopStart)
+        ) {
+            Text(
+                text = "왼쪽 위",
+                color = Color.White,
+            )
+        }
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Green)
+                .padding(16.dp)
+                .align(Alignment.TopCenter)
+        ) {
+            Text(
+                text = "중앙 위",
+                color = Color.White,
+            )
+        }
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Gray)
+                .padding(16.dp)
+                .align(Alignment.TopEnd)
+        ) {
+            Text(
+                text = "오른쪽 위",
+                color = Color.White,
+            )
+        }
+
+        Button(
+            onClick = {},
+            modifier = Modifier.align(Alignment.CenterStart)
+        ) {
+            Text(text = "중앙 왼쪽")
+        }
+
+        Button(
+            onClick = {},
+            modifier = Modifier.align(Alignment.Center)
+        ) {
+            Text(text = "중앙 중앙")
+        }
+
+        Button(
+            onClick = {},
+            modifier = Modifier.align(Alignment.CenterEnd)
+        ) {
+            Text(text = "중앙 오른쪽")
+        }
+
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Blue)
+                .padding(16.dp)
+                .align(Alignment.BottomStart)
+        ) {
+            Text(
+                text = "왼쪽 아래",
+                color = Color.White,
+            )
+        }
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Green)
+                .padding(16.dp)
+                .align(Alignment.BottomCenter)
+        ) {
+            Text(
+                text = "중앙 아래",
+                color = Color.White,
+            )
+        }
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Gray)
+                .padding(16.dp)
+                .align(Alignment.BottomEnd)
+        ) {
+            Text(
+                text = "오른쪽 아래",
+                color = Color.White,
+            )
+        }
+
+    }
+
 }
 
 @Composable
