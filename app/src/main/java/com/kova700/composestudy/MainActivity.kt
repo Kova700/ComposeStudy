376ae12a6b3596932eb1f60dfa8ac78d201ddabb
Tom <blak1002@gmail.com>
2023-12-15 11:59:07 +0900

    Drawer 예제

--- app/src/main/java/com/kova700/composestudy/MainActivity.kt
+++ app/src/main/java/com/kova700/composestudy/MainActivity.kt
@@ -34,7 +34,10 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
@@ -43,11 +46,15 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -56,7 +63,9 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -66,12 +75,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -91,16 +100,123 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeStudyTheme {
+                DrawerTest()
             }
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ComposeStudyTheme {
+fun DrawerTest() {
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    val screens = listOf(
+        Screen.Home,
+        Screen.Settings,
+        Screen.Phone,
+        Screen.Search,
+        Screen.Lock,
+    )
+
+    val selectedScreen: MutableState<Screen> = remember {
+        mutableStateOf(Screen.Home)
     }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "MyDrawer")
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+                            scope.launch { drawerState.open() }
+                        }
+                    ) {
+                        Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
+                    }
+                }
+            )
+        }
+
+    ) { paddingValues ->
+        ModalNavigationDrawer(
+            drawerState = drawerState,
+            modifier = Modifier.padding(paddingValues),
+            drawerContent = {
+                ModalDrawerSheet {
+                    screens.forEach { screen ->
+                        NavigationDrawerItem(
+                            icon = {
+                                Icon(
+                                    screen.icon,
+                                    contentDescription = screen.icon.name
+                                )
+                            },
+                            label = { Text(text = screen.name) },
+                            selected = screen == selectedScreen.value, //버튼 눌림 처리
+                            onClick = {
+                                scope.launch { drawerState.close() } //굳이 코루틴이 왜 필요할까.?
+                                selectedScreen.value = screen
+                            })
+                    }
+                }
+            },
+            content = {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(20.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    when(selectedScreen.value){
+                        Screen.Home -> HomeScreen()
+                        Screen.Settings -> SettingsScreen()
+                        Screen.Phone -> PhoneScreen()
+                        Screen.Search -> SearchScreen()
+                        Screen.Lock -> LockScreen()
+                    }
+
+                }
+            }
+        )
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    Text(text = "HomeScreen")
+}
+
+@Composable
+fun SettingsScreen() {
+    Text(text = "SettingsScreen")
+}
+
+@Composable
+fun PhoneScreen() {
+    Text(text = "PhoneScreen")
+}
+
+@Composable
+fun SearchScreen() {
+    Text(text = "SearchScreen")
+}
+
+@Composable
+fun LockScreen() {
+    Text(text = "LockScreen")
+}
+
+sealed class Screen(val name: String, val icon: ImageVector) {
+    object Home : Screen("Home", Icons.Default.Home)
+    object Settings : Screen("Settings", Icons.Default.Settings)
+    object Phone : Screen("Phone", Icons.Default.Phone)
+    object Search : Screen("Search", Icons.Default.Search)
+    object Lock : Screen("Lock", Icons.Default.Lock)
 }
 
 @Composable
@@ -114,7 +230,10 @@ fun APITest(url: String) {
             onClick = {
                 coroutineScope.launch {
                     val response = ktorClient.get(url)
-                    Log.d("로그", "MainActivity: requestApi() - response : ${response.body<String>()}")
+                    Log.d(
+                        "로그",
+                        "MainActivity: requestApi() - response : ${response.body<String>()}"
+                    )
                 }
             }
         ) {
